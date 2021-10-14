833aefa49b2c832e3cb738d73b8b59645399c7f9
Nebulizer1213 <fixingg@gmail.com>
2021-10-14 17:28:07 -0400

    update

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup, find_packages, Extension
+import sys
+import os
+
+if sys.version_info < (3, 7):
+    raise RuntimeError("aiohttp-ratelimiter requires python 3.7 or later.")
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
 
 
 def get_long_description():
@@ -16,6 +26,32 @@ classifiers = [
 ]
 
 
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                if extension.language == "c++":
+                    ext = ".cpp"
+                else:
+                    ext = ".c"
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
+
+
+extensions = [Extension("aiohttplimiter.utils", ["aiohttplimiter/utils.pyx"])]
+
+CYTHONIZE = bool(int(os.getenv("CYTHONIZE", 0))) and cythonize is not None
+
+if CYTHONIZE:
+    compiler_directives = {"language_level": 3, "embedsignature": True}
+    extensions = cythonize(extensions, compiler_directives=compiler_directives)
+else:
+    extensions = no_cythonize(extensions)
+
 setup(
     name="aiohttp-ratelimiter",
     version=VERSION,
@@ -30,7 +66,7 @@ setup(
     keywords="",
     packages=find_packages(),
     install_requires=["aiohttp"],
-    ext_modules=[Extension("aiohttplimiter.utils", ["aiohttplimiter/utils.pyx"])],
+    ext_modules=extensions,
     package_data={"aiohttplimiter": ["*.pyi"]},
     setup_requires=["Cython"]
 )
