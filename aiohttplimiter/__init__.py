18983fd18c83ab0e1409f18927b4db77ed5aa8a4
Alan Bogarin <bogarin01alan@gmail.com>
2025-03-05 17:14:30 -0300

    Add __version__ attr to the package

--- aiohttplimiter/__init__.py
+++ aiohttplimiter/__init__.py
@@ -2,3 +2,5 @@ from aiohttplimiter.limiter import *  # default_keyfunc, Allow, RateLimitExceede
 from aiohttplimiter.memory_limiter import *  # Limiter
 from aiohttplimiter.memcached_limiter import *  # MemcachedLimiter
 from aiohttplimiter.redis_limiter import *  # RedisLimiter
+
+__version__ = "4.1.2"
